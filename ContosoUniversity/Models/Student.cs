4e49b2e854a8be4a550af53b27f7125c07afde80
Jan-MarcusSivadi <jmarcus@hot.ee>
2023-09-08 11:20:49 +0300

    Scaffold Student and Course Model into Views, Controllers

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -6,5 +6,5 @@ public class Student
     public string LastName { get; set; }
     public string FirstMidName { get; set; }
     public DateTime EnrollmentDate { get; set; }
-    public ICollection<Enrollment> Enrollments { get; set; }
+    public ICollection<Enrollment>? Enrollments { get; set; }
 }
